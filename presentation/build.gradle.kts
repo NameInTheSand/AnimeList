6e12e1a1989623e012087e470590d25405496ad3
Sam <semernnaduev@gmail.com>
2025-01-21 12:04:06 +0100

    Refactor: Modularize and Update Dependencies
    This commit introduces a modular architecture and updates several dependencies. Key changes include:
    
    -   Modularization:
        - Creates `presentation`, `domain`, and `data` modules.
        - Moves code from the `app` module into the new `presentation` module.
    -   New files and directories:
        - Adds `.gitignore` files for the new modules: `presentation`, `domain`, and `data`.
        - Adds `proguard-rules.pro` for presentation module.
        - Creates `build.gradle.kts` files for `domain` and `data`.
    -   Dependency updates:
        - Updates `ktor` to version 3.0.3
        - Updates `kotlinx_serialization` to version 1.7.1
        - Adds `ktorfit`, `paging`, `koin` and `coroutines_test` dependencies
        - Adds `kotlinx_serialization`, `junit`, `espresso`, and `material` dependencies
        - Renames `Color.kt`, `Type.kt` and `Theme.kt` files and packages.
        - Updates `MainActivity.kt` package and name.
    -   Adds new project includes: `presentation`, `domain`, `data`
    -   Adds `serialization` plugin
    - Adds  `java-library`, `kotlin-jvm` and `android-library` plugins.
    - Removes unused dependencies.
    - Creates `AndroidManifest.xml` for presentation module.

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.serialization)
+}
+
+android {
+    namespace = "com.ukrdroiddev.presentation"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 25
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    //Modules
+    implementation(project(":domain"))
+
+    //UI
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Splash screen
+    implementation(libs.splashscreen)
+
+    //Navigation
+    implementation(libs.navigation)
+
+    //Serialization
+    implementation(libs.kotlinx.serialization)
+
+    //Koin
+    implementation(libs.koin.android)
+
+    //Compose
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    //Paging
+    implementation(libs.paging)
+    implementation(libs.paging.compose)
+
+    //Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+}
